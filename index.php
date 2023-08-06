3a8c9dcfbfaf43ad00bc59a802c4b54de6981763
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2023-08-06 13:03:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    $funny_Text = "Greetings";
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,7 +14,7 @@
                 <img src="Assets/IMG/root.gif" height="420" width="420">
             </div>
             <div id="textstuffs">
-                <h1><?php echo "Greetings";?></h1> 
+                <h1><?=$funny_Text?></h1> 
                 <button class="button button1" onclick="window.location.href='https://twitter.com/obimydude'">..My Twitter..</button>
                 <div><button class="button button2" onclick="window.location.href='https://github.com/Obimydudee'">..My Github..</button></div>
             </div>
